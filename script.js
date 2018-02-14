71683d56707dc17236300ee707ba9d73c183e651
crocsg <34553036+crocsg@users.noreply.github.com>
2018-02-14 15:37:43 +0100

    test

--- script.js
+++ script.js
@@ -279,10 +279,10 @@ $(document).ready( function() {
 						}
 						
 						// move printer head
-						//gcode += gcodeMoveTo(null, null, headDownPosition)
-						//gcode += gcodeMoveTo(null, null, headUpPosition)
-						gcode += GCODEdown;
-						gcode += GCODEup;
+						gcode += gcodeMoveTo(null, null, headDownPosition)
+						gcode += gcodeMoveTo(null, null, headUpPosition)
+						//gcode += GCODEdown;
+						//gcode += GCODEup;
 					}
 				}
 			}
